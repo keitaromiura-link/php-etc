64fed43f372fd4f2999777b610efad534d7e386f
keitaromiura-link <k.miura@reservelink.co.jp>
2017-01-26 16:26:31 +0900

    ディレクトリの処理を追加

--- bin/application_make.sh
+++ bin/application_make.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+abs_dirname() {
+  local cwd="$(pwd)"
+  local path="$1"
+
+  while [ -n "$path" ]; do
+    cd "${path%/*}"
+    local name="${path##*/}"
+    path="$(readlink "$name" || true)"
+  done
+
+  pwd -P
+  cd "$cwd"
+}
+
+script_dir="$(abs_dirname "$0")"
 
 # 引数の存在確認
 # なければエラーで終了
@@ -22,28 +37,28 @@ fi
 
 # 引数の重複確認
 # ../application/以下に同様のディレクトリが無いことを確認する
-if [[ -e "../application/$1" ]]; then
+if [[ -e "$script_dir../application/$1" ]]; then
 	echo "すでにアプリケーションが存在します" 1>&2
 	exit 1
 fi
 
 # ../vendor/codeigniter/framework/applicationディレクトリを名前を変えて../applicationディレクトリにコピーする
 echo "making directory ../application/$1" 1>&2
-cp -r ../vendor/codeigniter/framework/application ../application/$1 1>&2
+cp -r $script_dir/../vendor/codeigniter/framework/application $script_dir/../application/$1 1>&2
 echo "done;" 1>&2
 
 #  ./vendor/codeigniter/framework/index.phpファイルを名前を変えて ../pubilc_htmlディレクトリにコピーする
 echo "making file ../public_html/${1}.php" 1>&2
-cp ../vendor/codeigniter/framework/index.php ../public_html/${1}.php 1>&2
+cp $script_dir/../vendor/codeigniter/framework/index.php $script_dir/../public_html/${1}.php 1>&2
 echo "done;" 1>&2
 
 echo "checking file ../public_html/${1}.php" 1>&2
 #  ./index.phpのファイルの中身を変更する
 # $system_path = 'system'; → $system_path = '../vendor/codeigniter/framework/system';
-sed -i -e "s#\$system_path = 'system';#\$system_path = '../vendor/codeigniter/framework/system';#g" ../public_html/${1}.php 1>&2
+sed -i -e "s#\$system_path = 'system';#\$system_path = '../vendor/codeigniter/framework/system';#g" $script_dir/../public_html/${1}.php 1>&2
 echo "." 1>&2
 # $application_folder = 'application'; → $application_folder = '../application/XXXXX';
-sed -i -e "s#\$application_folder = 'application';#\$application_folder = '..\/application\/${1}';#g" ../public_html/${1}.php 1>&2
+sed -i -e "s#\$application_folder = 'application';#\$application_folder = '..\/application\/${1}';#g" $script_dir/../public_html/${1}.php 1>&2
 echo "done;" 1>&2
 
 echo "finished !"
